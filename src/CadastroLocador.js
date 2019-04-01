e6833164b3aa482a77f87bb32d9b23492bcba741
cin_laraujo <cin_laraujo@uolinc.com>
2019-04-01 17:52:47 -0300

    Upload pagina teste para futuras validações de campos

--- src/CadastroLocador.js
+++ src/CadastroLocador.js
@@ -8,19 +8,42 @@ class CadastroLocador extends Component {
       <div class="centro">
         <form class="formulario-teste">
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="nome_fantasia">Nome fantasia:</label>
+            <input type="text" class="form-control" id="nome_fantasia" placeholder="Digite o nome fantasia..."/>
           </div>
           <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            <label for="razao_social">Razão social</label>
+            <input type="text" class="form-control" id="razao_social" placeholder="Digite a razão social..."/>
+          </div>
+          <div class="form-group">
+            <label for="inscricao_estadual">Inscrição estadual</label>
+            <input type="text" class="form-control" id="inscricao_estadual" placeholder="Digite a inscrição estadual..."/>
+          </div>
+          <div class="form-group">
+            <label for="cnpj">cnpj</label>
+            <input type="text" class="form-control" id="cnpj" placeholder="Digite o cnpj..."/>
+          </div>
+          <div class="form-group">
+            <label for="endereco">Endereço</label>
+            <input type="text" class="form-control" id="endereco" placeholder="Digite o endereço..."/>
+          </div>
+          <div class="form-group">
+            <label for="telefone">Telefone</label>
+            <input type="text" class="form-control" id="telefone" placeholder="Digite o telefone..."/>
+          </div>
+          <div class="form-group">
+            <label for="plano">Plano</label>
+            <input type="text" class="form-control" id="plano" placeholder="Digite o plano..."/>
+          </div>
+          <div class="form-group">
+            <label for="perfil">Perfil</label>
+            <input type="text" class="form-control" id="perfil" placeholder="Digite o perfil..."/>
           </div>
           <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <input type="checkbox" class="form-check-input" id="“Eu li e concordo com os termos de uso"/>
+            <label class="form-check-label" for="termos_de_uso">Eu li e concordo com os termos de uso</label>
           </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
+          <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
       </div>
     );
