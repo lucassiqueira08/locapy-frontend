243d3f69bbafd92a1f693b96087d2587b043cdf0
cin_laraujo <cin_laraujo@uolinc.com>
2019-04-04 18:11:27 -0300

    formulario, importação do jquery e toast. Modelo do ajax semi-pronto(falta testar no back-end)

--- src/CadastroLocador.js
+++ src/CadastroLocador.js
@@ -1,49 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import {ToastsContainer, ToastsStore} from 'react-toasts';
+
 class CadastroLocador extends Component {
+  componentDidMount(){
+    //Metodo que é executado após o componente ser rendenizado
+  }
+  handleDelete(e, i){
+    e.preventDefault;
+
+  }
+  PostLocador(){
+    // debugger;
+    var url = '/cadastro/locador';
+    var data = {};
+    data.nome_fantasia = $('#nome_fantasia').text();
+    data.razao_social = $('#razao_social').text();
+    data.inscricao_estadual = $('#inscricao_estadual').text();
+    data.cnpj = $('#cnpj').text();
+    data.endereco = $('#endereco').text();
+    data.telefone = $('#telefone').text();
+    data.perfil = $('#perfil').text();
+
+    console.log(data)
+
+    ToastsStore.success("Hey, you just clicked!")
+
+    // $.ajax({
+    //   type: "POST",
+    //   url: url,
+    //   data: JSON.stringify(data),
+    //   success: function(){
+    //     alert("Locador cadastrado com sucesso!")
+    //   },
+    //   error: function(){
+    //     alert("Erro!")
+    //   },
+    //   dataType: JSON
+    // });
+  }
+
   render() {
     return (
-      <div class="centro">
-        <form class="formulario-teste">
-          <div class="form-group">
-            <label for="nome_fantasia">Nome fantasia:</label>
-            <input type="text" class="form-control" id="nome_fantasia" placeholder="Digite o nome fantasia..."/>
+      <div className="centro" onSubmit="this.preventDefault()">
+        <form className="formulario">
+          <div className="form-group">
+            <label htmlFor="nome_fantasia">Nome fantasia:</label>
+            <input type="text" className="form-control" id="nome_fantasia" placeholder="Digite o nome fantasia..."/>
           </div>
-          <div class="form-group">
-            <label for="razao_social">Razão social</label>
-            <input type="text" class="form-control" id="razao_social" placeholder="Digite a razão social..."/>
+          <div className="form-group">
+            <label htmlFor="razao_social">Razão social</label>
+            <input type="text" className="form-control" id="razao_social" placeholder="Digite a razão social..."/>
           </div>
-          <div class="form-group">
-            <label for="inscricao_estadual">Inscrição estadual</label>
-            <input type="text" class="form-control" id="inscricao_estadual" placeholder="Digite a inscrição estadual..."/>
+          <div className="form-group">
+            <label htmlFor="inscricao_estadual">Inscrição estadual</label>
+            <input type="text" className="form-control" id="inscricao_estadual" placeholder="Digite a inscrição estadual..."/>
           </div>
-          <div class="form-group">
-            <label for="cnpj">cnpj</label>
-            <input type="text" class="form-control" id="cnpj" placeholder="Digite o cnpj..."/>
+          <div className="form-group">
+            <label htmlFor="cnpj">cnpj</label>
+            <input type="text" className="form-control" id="cnpj" placeholder="Digite o cnpj..."/>
           </div>
-          <div class="form-group">
-            <label for="endereco">Endereço</label>
-            <input type="text" class="form-control" id="endereco" placeholder="Digite o endereço..."/>
+          <div className="form-group">
+            <label htmlFor="endereco">Endereço</label>
+            <input type="text" className="form-control" id="endereco" placeholder="Digite o endereço..."/>
           </div>
-          <div class="form-group">
-            <label for="telefone">Telefone</label>
-            <input type="text" class="form-control" id="telefone" placeholder="Digite o telefone..."/>
+          <div className="form-group">
+            <label htmlFor="telefone">Telefone</label>
+            <input type="text" className="form-control" id="telefone" placeholder="Digite o telefone..."/>
           </div>
-          <div class="form-group">
-            <label for="plano">Plano</label>
-            <input type="text" class="form-control" id="plano" placeholder="Digite o plano..."/>
+          <div className="form-group">
+            <label htmlFor="perfil">Perfil</label>
+            <input type="text" className="form-control" id="perfil" placeholder="Digite o perfil..."/>
           </div>
-          <div class="form-group">
-            <label for="perfil">Perfil</label>
-            <input type="text" class="form-control" id="perfil" placeholder="Digite o perfil..."/>
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input" id=""/>
+            <label className="form-check-label" htmlFor="termos_de_uso">Eu li e concordo com os termos de uso</label>
           </div>
-          <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="“Eu li e concordo com os termos de uso"/>
-            <label class="form-check-label" for="termos_de_uso">Eu li e concordo com os termos de uso</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
+          <button type="submit" className="btn btn-primary" onClick ={this.PostLocador(this)}>Cadastrar</button>
+          <button   onClick={(e,i) => this.handleDelete(e,i)}>Click me</button>
+
+          <ToastsContainer store={ToastsStore}/>
         </form>
       </div>
     );
