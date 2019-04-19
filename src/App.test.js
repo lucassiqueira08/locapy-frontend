952060b331eb642df925745bba69134789e8a533
Lucas Siqueira <lalvessiqueira8@gmail.com>
2019-04-19 15:36:55 -0300

    develop merge

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CadastroLocador from './CadastroLocador';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<CadastroLocador />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
